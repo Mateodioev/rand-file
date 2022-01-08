405242cdd84efaf7602b98be2abe9d44ee922dc5
Mateo Ore <orehurcomito@gmail.com>
2022-01-08 15:09:12 -0500

    Added methods compare and randarray

--- src/Config/StringUtils.php
+++ src/Config/StringUtils.php
@@ -13,4 +13,19 @@ class StringUtils {
         return @str_replace($mark, $replace, $string);
     }
 
+    /**
+     * Obtener un elemento aleatorio de un array
+     */
+    public static function RandArray(array $arr)
+    {
+        return $arr[array_rand($arr)];
+    }
+
+    /**
+     * Comparar si existe una cadena en un array
+     */
+    public static function Compare(array $data, string $id): bool
+    {
+        return in_array($id, $data);
+    }
 }
